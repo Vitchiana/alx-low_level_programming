cc83531c2596177ad9c8812209bb1675db0a11cf
Victor <vitchiiana@gmail.com>
2023-03-27 06:09:05 -0700

    0. 98 Battery st.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,16 @@
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
-
+        }
 	_putchar('\n');
 }
